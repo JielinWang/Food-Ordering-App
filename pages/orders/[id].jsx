24c8e9f7f3b557890d512924c79372359e92ff07
jielin Wang <wangjielin777@gmail.com>
2023-01-23 16:14:41 -0600

    create mongoose schema

---
+++ pages/orders/[id].jsx
@@ -0,0 +1,8 @@
+import styles from "../../styles/Order.module.css";
+import Image from "next/image";
+
+const Order = () => {
+  return <div className={styles.container}></div>;
+};
+
+export default Order;
